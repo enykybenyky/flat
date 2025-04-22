82badb0c90584955b1d7db4ec836e175b6bae98d
VašeJméno <VEmail>
2025-04-22 20:11:48 +0200

    Updated config.py with corrected email sender

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
-
-EMAIL_SENDER = "tvuj.email@gmail.com"  # Zde vlož svůj email
-EMAIL_PASSWORD = "heslo nebo app password"  # Tvé app password pro Gmail
+EMAIL_SENDER = "nuedns@gmail.com"  # Zde vlož svůj email
+EMAIL_PASSWORD = "yses dbkm flzl ahlj"  # Tvé app password pro Gmail
 EMAIL_RECEIVER = "dnsnue@seznam.cz"
 MAX_RENT = 1800
 ROOMS = [1, 2]
